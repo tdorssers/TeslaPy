248b7be773a8e6c23cd8c90b86b9ac18132785d3
Tim Dorssers <tim.dorssers@xs4all.nl>
2026-02-20 20:50:28 +0100

    Integrated #174

--- teslapy/__init__.py
+++ teslapy/__init__.py
@@ -813,12 +813,11 @@ class Product(JsonDict):
     def command(self, name, **kwargs):
         """ Wrapper method for product command response error handling """
         response = self.api(name, **kwargs)['response']
-        # Normalize keys to lowercase for case-insensitive lookup of items
-        r = {k.lower(): v for k, v in response.items()}
-        # Expect to receive a 200 or 201 code here, else raise error
-        if r.get('code') in (200, 201):
-            return r.get('message')
-        raise ProductError(response)
+        if isinstance(response, str):
+            response = json.loads(response)
+        if (response.get('code') or response.get('Code')) in (200, 201):
+            return response.get('message')
+        raise ProductError(response.get('message') or repr(response))
 
 class BatteryTariffPeriodCost(
         namedtuple('BatteryTariffPeriodCost', ['buy', 'sell', 'name'])):
